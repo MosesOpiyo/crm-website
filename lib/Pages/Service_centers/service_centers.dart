d1dab70dc9877c2029edbe10b34014d9101f585d
MosesOpiyo <mosesopiyo54@gmail.com>
2023-07-11 22:22:22 +0300

    Redesign on service center pages

--- lib/Pages/Service_centers/service_centers.dart
+++ lib/Pages/Service_centers/service_centers.dart
@@ -34,6 +34,124 @@ class ServiceCentersBody extends StatefulWidget {
 }
 
 class _ServiceCentersBodyState extends State<ServiceCentersBody> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String center = '';
+  TextEditingController centerController = TextEditingController();
+
+  newCenter() {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: const Text("New Service Center"),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              content: StatefulBuilder(
+                builder: (context, setState) {
+                  var height = MediaQuery.of(context).size.height;
+                  var width = MediaQuery.of(context).size.width;
+
+                  return SizedBox(
+                      height: height - 400,
+                      width: width - 950,
+                      child: Column(children: [
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  padding:
+                                      const EdgeInsets.only(left: 2, bottom: 5),
+                                  width: 330,
+                                  alignment: Alignment.centerLeft,
+                                  child: const Row(
+                                    children: [
+                                      Text(
+                                        "Service Center Name",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "*",
+                                        style: TextStyle(color: Colors.red),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 330,
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0)),
+                                        labelText: 'Service Center Name'),
+                                    keyboardType: TextInputType.text,
+                                    controller: centerController,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const Padding(padding: EdgeInsets.only(top: 10)),
+                        Row(children: [
+                          const Spacer(),
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.blue),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          side: const BorderSide(
+                                              color: Colors.blue)))),
+                              onPressed: () {
+                                center = centerController.text;
+                                Serviceservice().newCenter(center);
+                                Navigator.of(ctx).pop();
+                                Navigator.pushNamed(
+                                    context, ServiceCenters.route);
+                              },
+                              child: const Text("Add",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ))),
+                          const Padding(padding: EdgeInsets.only(left: 10)),
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          side: const BorderSide(
+                                              color: Colors.blue)))),
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                              child: const Text("Cancel",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w400,
+                                  )))
+                        ])
+                      ]));
+                },
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -198,58 +316,57 @@ class _ServiceCentersBodyState extends State<ServiceCentersBody> {
                   ],
                 )),
             Padding(
-              padding: const EdgeInsets.only(right: 5, top: 10),
-              child: AppBar(
-                automaticallyImplyLeading: false,
-                title: Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: 40,
-                        child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.blue),
-                                shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                        side: const BorderSide(
-                                            color: Colors.blue)))),
-                            onPressed: () {},
-                            child: const Row(
-                              children: [
-                                Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),
-                                Text("Add Stock",
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w400,
-                                    ))
-                              ],
-                            )),
-                      )
-                    ],
-                  ),
-                ),
-                actions: [
-                  SizedBox(
-                    width: 330,
-                    height: 45,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                          labelText: 'Search'),
-                      keyboardType: TextInputType.text,
+              padding: const EdgeInsets.only(top: 10),
+              child: SizedBox(
+                width: size.width,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 40,
+                      margin: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.blue),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      side: const BorderSide(
+                                          color: Colors.blue)))),
+                          onPressed: () {
+                            newCenter();
+                          },
+                          child: const Row(
+                            children: [
+                              Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                              Text("Add Service Center",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ))
+                            ],
+                          )),
                     ),
-                  ),
-                ],
+                    const Spacer(),
+                    Container(
+                      margin: const EdgeInsets.only(right: 5),
+                      width: 330,
+                      height: 45,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            labelText: 'Search'),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Container(
